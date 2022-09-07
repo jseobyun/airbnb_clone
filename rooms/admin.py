a81c8555ac0084c4a46788ece9107662f643b714
jseobyun <whdtjq4144@gmail.com>
2022-09-07 09:01:55 +0900

    class8.5 photo admin, how to safely use html string to admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
+
 
 # Register your models here.
 
@@ -127,4 +129,9 @@ class RoomAdmin(admin.ModelAdmin):
 class PhotoAdmin(admin.ModelAdmin):
     """Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get__thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
