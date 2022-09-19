b7ad8085a353dcc5f24a14df3c4fb9911f54251a
jseobyun <whdtjq4144@gmail.com>
2022-09-20 08:59:56 +0900

    class10.0-10.2 introduction to urls, view, httpresponse, render, templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.shortcuts import render
 
+# from django.http import HttpResponse
+
+
 # Create your views here.
+def all_rooms(request):
+    now = datetime.now()
+    hungry = True
+    # return HttpResponse(content="hello")
+    context = {
+        "now": now,
+        "hungry": hungry,
+    }
+    return render(request, "all_rooms.html", context=context)
+
+
+1
